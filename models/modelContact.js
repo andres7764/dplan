642647994d08a91331df3393c64b3081e7ae8137
andres7764 <andres7764@hotmail.com>
2017-11-07 00:13:27 -0500

    new

--- models/modelContact.js
+++ models/modelContact.js
@@ -6,9 +6,21 @@ contacto = new mongoose.Schema({
   mail:               { type: String },
   date:               { type: Date, default: new Date()},
   activeSubscription: { type: Boolean, default: true},
-  description: 		  { type: String },
-  phone: 			  { type: String }
+  description: 		    { type: String },
+  phone: 			        { type: String }
 });
 
+customplan = new mongoose.Schema({
+  name:               { type: String  },
+  mail:               { type: String  },
+  phone: 			        { type: String  },
+  transp:             { type: Boolean },
+  food:               { type: Boolean },
+  hostal:             { type: Boolean },
+  qty:                { type: String  },
+  dateExit:           { type: Date    },
+  description:        { type: String  },
+});
 
 module.exports = mongoose.model('contactmodel',contacto);
+module.exports = mongoose.model('customplan', customplan);
